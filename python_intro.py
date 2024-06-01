2013c8c79911ff0894e701ff183ca1a8370281fe
yuricik <ycanasq@unal.edu.co>
2024-06-01 13:55:35 -0500

    Mi aplicacion Django Girls, primer commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+print('Hello, Django Girls!')
+#Para hacer los comentarios
+
+def hi(name):
+    print('Hi '+ name + '!')
+
+hi('ME')
+
+girls = ['Rachel','Monica', 'Phoebe', 'Ola', 'Your']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
